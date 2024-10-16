78d989db448b31b6c8e654de9ce10d53cee57fe0
kimbriah03 <kimbriah.alfrenar@gmail.com>
2024-10-16 13:13:53 -0400

    Added upload prescription form

--- app/upload-prescription/page.jsx
+++ app/upload-prescription/page.jsx
@@ -6,8 +6,38 @@ User only
 - Drag/Drog/Upload File
 */
 
+
+"use client";
+
+
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import UploadPres from "@/components/auth/UploadPres";
+
+
 const Page = () => {
-  return <div></div>;
+ return (
+   <div className="flex flex-col min-h-screen">
+     <NavBar />
+
+
+     <main>
+         <div className="w-full flex justify-center items-center overflow-hidden p-3">
+           <div className="shadow-lg p-8 w-[500px] rounded-xl bg-slate-50">
+             <h1 className="font-bold text-xl text-center p-4">Upload Prescription</h1>
+             <UploadPres />
+           </div>
+         </div>
+     </main>
+
+
+     <Footer />
+   </div>
+ );
 };
 
+
 export default Page;
+
+
+
